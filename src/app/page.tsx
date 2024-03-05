bb8765a1331a21e0e26c5420bf7f133f254786c0
Thiago Tallison <thiagotallison@gmail.com>
2024-03-05 12:28:12 -0300

    feat: main header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { MainHeader } from "@/components/header";
 import { Sidebar } from "@/components/sidebar";
 
 export default function Home() {
   return (
     <div className="min-h-screen min-w-screen grid grid-cols-[minmax(360px,412px)_1fr] p-2 gap-2">
       <Sidebar />
-      <main className="bg-muted rounded-lg p-4">Main content</main>
+      <main className="bg-muted rounded-lg p-4">
+        <MainHeader />
+      </main>
     </div>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,79 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Bell, ChevronLeft, ChevronRight, Users } from "lucide-react";
+import Image from 'next/image';
+
+export function MainHeader() {
+  return (
+    <header className="flex justify-between items-center">
+      <TooltipProvider>
+        <div className="flex items-center gap-2">
+          <Tooltip>
+            <TooltipTrigger asChild >
+              <button className="w-8 h-8 bg-card rounded-full flex items-center justify-center disabled:bg-card/20" disabled>
+                <ChevronLeft size={28} strokeWidth={1} />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent className="bg-card text-card-foreground">
+              <span>Voltar</span>
+            </TooltipContent>
+          </Tooltip>
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button className="w-8 h-8 bg-card rounded-full flex items-center justify-center disabled:bg-card/20">
+                <ChevronRight size={28} strokeWidth={1} />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent className="bg-card text-card-foreground">
+              <span>Avançar</span>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+
+        <div className="flex gap-2">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button className="w-8 h-8 bg-card rounded-full flex p-1 items-center justify-center">
+                <Bell size={18} />
+              </button>
+            </TooltipTrigger>
+
+            <TooltipContent className="bg-card text-card-foreground">
+              <span>Novidades</span>
+            </TooltipContent>
+          </Tooltip>
+
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button className="w-8 h-8 bg-card rounded-full flex p-1 items-center justify-center">
+                <Users size={18} />
+              </button>
+            </TooltipTrigger>
+
+            <TooltipContent className="bg-card text-card-foreground">
+              <span>Atividade de amigos</span>
+            </TooltipContent>
+          </Tooltip>
+
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button className="w-8 h-8 bg-card rounded-full flex p-1 items-center justify-center">
+                <Image
+                  src={"https://github.com/thiago-tallison.png"}
+                  alt="User profile"
+                  width={24}
+                  height={24}
+                  className="rounded-full" />
+              </button>
+            </TooltipTrigger>
+
+            <TooltipContent className="bg-card text-card-foreground">
+              <span>Thiago Tallison</span>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      </TooltipProvider>
+    </header>
+  );
+}
