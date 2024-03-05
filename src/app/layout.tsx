b320633ce4438b60005ce7e082db110ebf694428
Thiago Tallison <thiagotallison@gmail.com>
2024-03-05 10:26:42 -0300

    feat: add chadcn themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"]
@@ -17,8 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className + " antialiased"}>{children}</body>
+    <html lang="pt-br" suppressHydrationWarning>
+      <body className={inter.className + " antialiased"}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
