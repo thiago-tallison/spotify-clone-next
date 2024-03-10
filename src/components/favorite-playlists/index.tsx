a380772807cf8b2920da60ce7320603c75bce022
Thiago Tallison <thiagotallison@gmail.com>
2024-03-10 12:10:04 -0300

    refactor: replace multiple PlaylistItem to an array

--- src/components/favorite-playlists/index.tsx
+++ src/components/favorite-playlists/index.tsx
@@ -2,13 +2,12 @@ import { PlaylistItem } from "../playlist-item-main";
 
 export function RecentlyPlayed() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-2 px-4">
-      <PlaylistItem title="Red Hot Chilli Peppersashdiashdaisdhiasdasdsauasdasd" img="/rhcp.jpg" />
-      <PlaylistItem title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
-      <PlaylistItem title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
-      <PlaylistItem title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
-      <PlaylistItem title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
-      <PlaylistItem title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
+    <div className="grid grid-cols-2 xl:grid-cols-3 gap-2 px-4">
+      {Array.from({ length: 6 }).map(() => (
+        <PlaylistItem
+          key={Math.random()}
+          title="Red Hot Chilli Peppers" img="/rhcp.jpg" />
+      ))}
     </div>
   )
 }
