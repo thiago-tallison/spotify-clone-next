817363b586ef843eaee3b31a59d24b8f0fd48214
Thiago Tallison <thiagotallison@gmail.com>
2024-03-11 15:21:02 -0300

    refactor: add use client into main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { RecentlyPlayed } from "@/components/favorite-playlists";
 import { MainHeader } from "@/components/header";
 import { ScrollArea } from "@/components/ui/scroll-area";
